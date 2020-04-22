bac8b2d15703e1cf3ff7a3cb40c643b02f7407f0
Steven Leone <SALeone1999@yahoo.com>
2020-04-21 20:58:39 -0400

    Worked on updateLocation

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from flask import Flask, jsonify, request, redirect, url_for, abort
 from flask_restful import Resource
-from distance import distance
+import distance
 
 
 
 app = Flask(__name__)
+con = sqlite3.connect('record.sqlite3')
 
 @app.route('/updatelocation/<id>/<latitude>/<longitude>')
-def updatelocation(id, latitude, longitude):
+def updatelocation(uid, latitude, longitude):
+	#Update Database
+	cur = con.cursor()
+	cur.execute('UPDATE users SET latitude = ?, longitude = ? WHERE id = ?', (latitude, longitude, uid))
+	con.commit()
+	#Search for users in contact
+	cur.execute('SELECT zipcode from users WHERE id = ?', (uid,))
+	zipcode, = cur.fetchone()
 	return 0
+
 
 if __name__ == '__main__':
 	app.debug = True
