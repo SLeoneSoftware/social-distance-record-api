5ff9c659c24f097afbc683110d07cb8553ccb367
Steven Leone <SALeone1999@yahoo.com>
2020-04-24 22:14:55 -0400

    Created Query for positive cases

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-con = sqlite3.connect('record.sqlite3')
 
 @app.route('/updatelocation/<id>/<latitude>/<longitude>')
 def updatelocation(uid, latitude, longitude):
 	#Update Database
+	con = sqlite3.connect('record.sqlite3')
 	cur = con.cursor()
 	cur.execute('UPDATE users SET latitude = ?, longitude = ? WHERE id = ?', (latitude, longitude, uid))
 	con.commit()
 	#Search for users in contact
-	cur.execute('SELECT zipcode from users WHERE id = ?', (uid,))
+	cur.execute('SELECT zipcode FROM users WHERE id = ?', (uid,))
 	zipcode, = cur.fetchone()
-	cur.execute('SELECT latitude, longitude, id from users WHERE zipcode = ?', (zipcode))
+	cur.execute('SELECT latitude, longitude, id FROM users WHERE zipcode = ?', (zipcode))
 	while True:
 		row = cur.fetchone()
 		if row == None:
@@ -35,10 +35,16 @@ def updatelocation(uid, latitude, longitude):
 			cur2.execute('INSERT INTO contacted (user, contacteduser, datemark, timemark) VALUES (?, ?, ?)', (uid, uid_two, date, time))
 			cur2.execute('INSERT INTO contacted (user, contacteduser, datemark, timemark) VALUES (?, ?, ?)', (uid_two, uid, date, time))
 	con.commit()
+	con.close()
 	return 0
 
 @app.route('/testedpositive/<id>')
 def testedpositive(uid):
+	con = sqlite3.connect('record.sqlite3')
+	cur = con.cursor()
+	cur.execute('SELECT contacted.contacteduser, contacted.datemark, contacted.timemark, user.email FROM contacted JOIN user on contacted.contacteduser = user.id  WHERE contacted.user = uid')
+	while True:
+	con.close()
 	return 0
 
 if __name__ == '__main__':
