e898e2c9331991bc66891428cecbcb4599882ca0
Steven Leone <SALeone1999@yahoo.com>
2020-04-27 19:58:24 -0400

    Fixed a bug on both CHECK constraints

--- users.sql
+++ users.sql
@@ -4,12 +4,13 @@
 --
 
 
+
 create table IF NOT EXISTS users(
 id integer NOT NULL,
-firstname varchar(255),
-email  NVARCHAR(320),
-phone varchar CHECK (phone LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-zipcode integer CHECK (zipcode LIKE '[0-9][0-9][0-9][0-9][0-9]'),
+firstname TEXT,
+email  TEXT,
+phone TEXT CHECK (phone NOT LIKE '%[^0-9]%') CHECK (length(phone) == 10),
+zipcode TEXT CHECK (zipcode NOT LIKE '%[^0-9]%') CHECK (length(zipcode) == 5),
 latitude integer NOT NULL,
 longitude integer NOT NULL,
 PRIMARY KEY(id)
